62e2fb96c7c3088551f67349f67ee7d18bf9b0b9
Aluno <aluno@band.net>
2016-03-09 22:18:59 -0300

    Forms

--- MenuCheff_Cliente/Form1.cs
+++ MenuCheff_Cliente/Form1.cs
@@ -15,6 +15,28 @@ namespace MenuCheff_Cliente
 		public Form1()
 		{
 			InitializeComponent();
+
+			//Dictionary<int, string> lista = new Dictionary<int, string>();
+			comboBox1.Items.Add(new Produto(3, "batatas").name);
+			comboBox1.Items.Add(new Produto(4, "banana").name);
+			comboBox1.Items.Add(new Produto(7, "sal").name);
+			comboBox1.Items.Add(new Produto(9, "lula").name);
+			comboBox1.Items.Add(new Produto(31, "dilma").name);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(comboBox1.SelectedItem);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Remove(listBox1.SelectedItem);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
